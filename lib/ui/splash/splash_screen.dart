dc20e1ef930d9019a2866ee3ac64415e866a1bbf
rafael.actt <rafael.actt@gmail.com>
2025-04-18 23:09:08 +0000

    Splashscreen

---
+++ lib/ui/splash/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/ui/_core/app_colors';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: Stack(
+        children: [
+          Image.asset('assets/banners/banner_splash.png'),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 32.0,
+                children: [
+                  Image.asset('assets/logo.png', width: 192),
+                  Column(
+                    children: [
+                      Text(
+                        "Um parceiro inovador para sua",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        'melhor experiência culinária',
+                        style: TextStyle(color: AppColors.mainColor),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(onPressed: () {}, child: Text('Bora')),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
